f5fc18beae2fe2097a9b55061170dfba31419c82
AndreiFerradesign <114855224+AndreiFerradesign@users.noreply.github.com>
2022-12-12 21:20:01 +0300

    Написаны Unit тесты

---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,54 @@
+//
+//  MoviesLoaderTests.swift
+//  MoviesLoaderTests
+//
+//  Created by Andrei on 12.12.2022.
+//
+
+import XCTest
+@testable import MovieQuiz // импортируем приложение для тестирования
+
+class MoviesLoaderTests: XCTestCase {
+    
+    func testSuccessLoading() throws {
+        
+        let stubNetworkClient = StubNetworkClient(emulateError: false)
+        let loader = MoviesLoader(networkClient: stubNetworkClient)
+        let expectation = expectation(description: "Loading expectation")
+        
+        loader.loadMovies { result in
+            // Then
+            switch result {
+            case .success(let movies):
+                // сравниваем данные с тем, что мы предполагали
+                expectation.fulfill()
+            case .failure(_):
+                // мы не ожидаем, что пришла ошибка; если она появится, надо будет провалить тест
+                XCTFail("Unexpected failure") // эта функция проваливает тест
+            }
+        }
+       
+       waitForExpectations(timeout: 1)
+    }
+    
+    func testFailureLoading() throws {
+       
+            let stubNetworkClient = StubNetworkClient(emulateError: true)
+            let loader = MoviesLoader(networkClient: stubNetworkClient)
+            let expectation = expectation(description: "Loading expectation")
+            
+            loader.loadMovies { result in
+                // Then
+                switch result {
+                case .failure(let error):
+                    XCTAssertNotNil(error)
+                    expectation.fulfill()
+                case .success(_):
+                    XCTFail("Unexpected failure")
+                }
+            }
+            
+            waitForExpectations(timeout: 1)
+        }
+}
+
